2635f864c1322bde221a823e3555097972e7b82d
Marat1206 <marikzhekov@gmail.com>
2021-05-22 22:04:18 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// const firstnumber = prompt('Введите первое число');
+// const secondnumber = prompt ('Введите  второе число');
+// const result = firstnumber * secondnumber;
+// alert('Результат умножения: ' +  result );
+const surname = prompt('Введите вашу фамилию');
+const name = prompt('Введите ваше имя');
+const middlename = prompt('Введите ваше отчество');
+const space = " ";
+const result = space + surname + space + name + space + middlename;
+alert('Ваши данные:' + result)

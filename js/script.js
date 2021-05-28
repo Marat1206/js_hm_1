b25134442e76b37160218d2ca5080e63e2127c06
Marat1206 <marikzhekov@gmail.com>
2021-05-28 15:57:02 +0300

    home_work_3

--- js/script.js
+++ js/script.js
@@ -2,9 +2,41 @@
 // const secondnumber = prompt ('Введите  второе число');
 // const result = firstnumber * secondnumber;
 // alert('Результат умножения: ' +  result );
-const surname = prompt('Введите вашу фамилию');
-const name = prompt('Введите ваше имя');
-const middlename = prompt('Введите ваше отчество');
-const space = " ";
-const result = space + surname + space + name + space + middlename;
-alert('Ваши данные:' + result)
+// // const surname = prompt('Введите вашу фамилию');
+// // const name = prompt('Введите ваше имя');
+// // const middlename = prompt('Введите ваше отчество');
+// // const space = " ";
+// // const result = space + surname + space + name + space + middlename;
+// // alert(':' + result)
+
+// --------------------------------hw_3-------------------------------------
+
+// Boolean(Number(’10’)) + 1; = 2
+// ‘sub ’ + Number(false); = sub0
+// 16  *  ‘          91      ‘ = 1456
+// true-70 = -69
+// Number(1 + String(1)) + 1; = 12
+
+
+
+// const hours = 60;
+// const sec = 60;
+// const result = hours * sec;
+// console.log(result);
+// let question = prompt('сколько часов вы проводите за ПК в сутки? ');
+// console.log( typeof question)
+// const result2 = result * question;
+// alert('Вы проводите: ' + result2 + ' ' + 'секунд за компьютером в сутки ');
+
+
+var num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num++;
+num--
+alert(num);
+
+
+
